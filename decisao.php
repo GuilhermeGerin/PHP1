f097f717be6302c23955fd0bf84117bc635b4b7d
GuilhermeGerin <111442222+GuilhermeGerin@users.noreply.github.com>
2022-09-13 09:32:05 -0300

    Add files via upload

---
+++ decisao.php
@@ -0,0 +1,19 @@
+<?php
+/*
+$n = 1;
+if ($n==0)
+    print "Somente Zero";
+else
+    {
+    print "não zero";
+    print "<br>acabou o programa";
+    }
+*/
+
+$n = 1;
+($n==0)? print "zero" : print "não zero";
+
+print ($n==0)? "zero" : "não zero";
+
+
+?>
